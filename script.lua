844cc73bcde2389de1141a1516afef1f51880eb4
Smartmaninnit <alanazain8@gmail.com>
2024-12-12 23:08:35 -0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -3,6 +3,10 @@ local Window = OrionLib:MakeWindow({Name = "Fish It!", HidePremium = false, Save
 
 local MainTab = Window:MakeTab({Name = "Main", Icon = "rbxassetid://4483345998"})
 local TpTab = Window:MakeTab({Name = "Teleports", Icon = "rbxassetid://4483345998"})
+local CreditsTab = Window:MakeTab({Name = "Credits", Icon = "rbxassetid://4483345998"})
+
+CreditsTab:AddLabel("Made by yoyoyoy")
+CreditsTab:AddLabel("discord.gg/Gjr5vEes")
 
 getgenv().autoFarmEnabled = false
 local platform = nil
@@ -26,7 +30,7 @@ MainTab:AddToggle({
             platform.Anchored = true
             platform.Position = game.Players.LocalPlayer.Character.HumanoidRootPart.Position - Vector3.new(0, 3, 0)
             platform.Parent = workspace
-            
+
             spawn(function()
                 while getgenv().autoFarmEnabled do
                     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-759.0910034179688, 24.309707641601562, 429.12823486328125)
@@ -40,22 +44,3 @@ MainTab:AddToggle({
         end
     end
 })
-
-local locations = {
-    ["Starter Island"] = CFrame.new(23.435884475708008, 4.625000953674316, 2868.347412109375),
-    ["Kohana Island"] = CFrame.new(-842.8712158203125, 55.500057220458984, 146.21389770507812),
-    ["Kohana Volcano"] = CFrame.new(-606.581787109375, 59.000057220458984, 105.82990264892578),
-    ["Coral Reefs"] = CFrame.new(-2853.76318359375, 47.499996185302734, 1988.1397705078125),
-    ["The Depths"] = CFrame.new(2002.4705810546875, 12.10128402709961, 1385.3233642578125)
-}
-
-for name, cf in pairs(locations) do
-    TpTab:AddButton({
-        Name = name,
-        Callback = function()
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = cf
-        end
-    })
-end
-
-MainTab:AddLabel("Press T to disable autofarm")
